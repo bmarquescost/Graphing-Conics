51b3f1089b39b37cccd15b4c10c85e96f815e4a9
Guibi0 <gmtri@hotmail.com>
2020-06-10 06:07:31 -0300

    Plotando Tudo o o o

--- ConicDetermination.js
+++ ConicDetermination.js
@@ -1,3 +1,4 @@
+  
 function decToFrac(value, donly = true) {
     let tolerance = 1.0E-10; 
     let h1 = 1;
@@ -202,12 +203,16 @@ function auxPrintEquation(coef, add, string) {
     if (coef > 0) {
         temp = decToFrac(coef, false)
         if (string) string += "+ "
-        if (temp != 1) string += `${temp}`    
+        if (add == " ") string += `${temp}`    
+        if (temp != 1) string += `${temp}`
         string += `${add}`    
     }
     else if (coef < 0) {
         temp = decToFrac(-coef, false)
-        if (temp != 1) string += `- ${temp}`    
+        if (temp != 1) string += `- ${temp}`
+        else { 
+            if (add == " ") string += `- ${temp}`    
+        }  
         string += `${add}`    
     }
 
@@ -231,10 +236,13 @@ function printEquation(a, b, c, d, e, f, x, y) {
 }
 
 function mmc(a,b,c,d,e,f) {
-    let denominador;
 
     let coefs = [a, b, c, d, e, f]
-    coefs = coefs.map( coefieciente => (Math.abs(coefieciente)))
+    for (let i = 0; i < 6; i++) {
+        if (parseInt(coefs[i]) != coefs[i]) return coefs
+    }
+
+    coefs = coefs.map(coefieciente => (Math.abs(coefieciente)))
     coefs = coefs.sort((a, b) => a - b);
 
     let i = 0
@@ -244,17 +252,16 @@ function mmc(a,b,c,d,e,f) {
     coefs = [a, b, c, d, e, f]
 
     for (div = 2; num > 1; div++) {
-        if (num % div != 0) continue;
+        if (num % div != 0) continue
         
         while (num % div == 0) {
-            num /= div;         
+            num /= div
             if ((coefs[0] % div == 0) && (coefs[1] % div == 0) && (coefs[2] % div == 0) && (coefs[3] % div == 0) && (coefs[4] % div == 0) && (coefs[5] % div)== 0) {
                 for (let i = 0; i < 6; i++) coefs[i] /= div
             }             
         }
     }
 
-    Math.least
     return coefs
 }
 
@@ -371,63 +378,66 @@ function graph() {
     if (type > 5) findElements(type, a2,b2,c2,d2,e2,f2)
 
     plotgraph(a2, b2, c2, d2, e2, f2, type);
+    // plotgraph(a, b, c, d, e, f, type); ///////////////////////////////////////////////SE CHAMAR AS DUAS DE NAO PRINTA AS DUAS 
+
     // Criando gráfico
     // board.create('conic', [4, 7, -9, -2, 6, 3]);
     // board.create('conic', [3, 8, -24, 0, 0, 0])
 }
+/////////////////////////////////////////////////////////////////////////////////////////DAQUI PRA BAIXO MEXI EM TUDO
+//Confere se eh diferença de quadrados pra facilitar plotagem das restas concorrentes
+const isDiffSquares = (a, b, c, d, e, f, type) => {
+    
+    if (!b && !d && !e) {
+        if (a * c < 0) {
+            return 1
+        }
+        else return 0
+    }
+    else return 0
+}
+
+//Confere se eh quadrado perfeito pra facilitar plotagem da retas paralelas
+const isPerfctSquare = (a, b, c, d, e, f, type) => {
+    
+    if (!d && !e && (f <= 0)) {
+        if ((a > 0) && (c > 0)) {
+            if (b < 0) {
+                if (b = -2 * Math.sqrt(a * c)) {
+                    return 1
+                } 
+                else return 0
+            }
+            else if (b > 0) {
+                if (b = 2 * Math.sqrt(a * c)) {
+                    return 1
+                } 
+                else return 0
+            }
+            else return 0
+        }
+        else return 0
+    }
+    else return 0
+}
+
+//Confere se eh algum caso do tipo x = k ou y = k, auxiliando na plotagem de retas coinscidentes e reta
+//0: nao, 1: caso com x, 2: caso com y
+const isBaseCase = (a, b, c, d, e, f, type) => {
+    
+    if (!a && !b && !c && d && !e) return 1 //Caso x = k = -f/d
+    if (a && !b && !c && !d && !e) return 2 //Caso x² = 0
+    if (!a && !b && !c && !d && e) return 3 //Caso y = k = -f/e
+    if (!a && !b && c && !d && !e) return 4 //Caso y² = 0
+}
+// http://www.onemathematicalcat.org/Math/Precalculus_obj/conicDiscriminant.htm
+// https://people.richland.edu/james/lecture/m116/conics/conics.html
+// me baseei ai pra pensar nessas funções
 
 const plotgraph = (a, b, c, d, e, f, type) => {
-    board.create('conic', [a, c, f, b/2, d/2, e/2]);
-    let y0 = 0;
 
     alert(type);
 
-
-    switch (type) {
-        case 0:
-            break;
-        case 1:
-            board.create('point', [0, 0])
-            break;
-        case 4: // -> Precisamos
-        // case 2:
-        //     board.create('line', []);
-        //     break;
-        // case 9: --> Desnecessário
-        default:
-            board.create('conic', [a, c, f, b/2, d/2, e/2]);
-            break;
-    }
-}
-var board = JXG.JSXGraph.initBoard('jgxbox1', {boundingbox: [-10, 10, 10, -10], axis:true});
-/*
-
-43
-123
-13
-23
-4324
-2
-
--------
-
-4
-0
--9
-0
-0
--36
-
-------
-
-4
--4
-7
-12
-6
--9
-
-*/
 
 /* Determine the type of the conic given her coefficientes AX² + BXY + CY² + DX + EY + F = 0
     0 -> Empty
@@ -440,4 +450,218 @@ var board = JXG.JSXGraph.initBoard('jgxbox1', {boundingbox: [-10, 10, 10, -10], 
     7 -> Hyperbola
     8 -> Parabola
     9 -> Cirfunferemce
+*/
+    switch (type) {
+        case 0: //empty
+            break;
+        case 1:
+            board.create('point', [0, 0])
+            break;
+        case 2: //2 retas identicas
+            let isBase2 = isBaseCase(a, b, c, d, e, f)
+            let isPerfSqr2 = isPerfctSquare(a, b, c, d, e, f)
+            console.log(a,b,c,d,e,f)
+            if (isBase2) {
+                if (isBase2 == 1) { // x = -f/d
+                    board.create('line',[[-(f/d),0],[-(f/d),1]]);
+                }
+                else if (isBase2 == 2) { //x² = 0 ou x = -f/a
+                    board.create('line',[[-(f/a),0],[-(f/a),1]]);
+                }
+                else if (isBase2 == 3) { // y = -f/e
+                    board.create('line',[[0,-(f/e)],[1,-(f/e)]]);
+                }
+                else { //y² = 0 ou y = -f/c
+                    board.create('line',[[0,-(f/c)],[1,-(f/c)]]);
+                }
+            }
+            else if (isPerfSqr2) {
+                if (b > 0) { //(ax + cy)² = -f
+                    y0NegativeSqrt = -(Math.sqrt(-f)/c)
+                    y0PositiveSqrt = (Math.sqrt(-f)/c)
+                    y1NegativeSqrt = -(a + Math.sqrt(-f))/c
+                    y1PositiveSqrt = (Math.sqrt(-f) - a)/c
+
+                    board.create('line',[[0,y0NegativeSqrt],[1,y1NegativeSqrt]]);
+                }
+                else { //(ax - cy)² = -f
+                    y0NegativeSqrt = -(Math.sqrt(-f)/c)
+                    y0PositiveSqrt = (Math.sqrt(-f)/c)
+                    y1NegativeSqrt = (a - Math.sqrt(-f))/c
+                    y1PositiveSqrt = (a + Math.sqrt(-f))/c
+
+                    board.create('line',[[0,y0NegativeSqrt],[1,y1NegativeSqrt]]);
+                }
+            }
+            // else /////////////////////////////////////////////////////////EU ACHO QUE NAO TEM CASO DE RETAS IDENTICAS QUE NAO SEJA CASO BASE OU QUADRADO PERFEITO
+// http://www.onemathematicalcat.org/Math/Precalculus_obj/conicDiscriminant.htm
+// https://people.richland.edu/james/lecture/m116/conics/conics.html
+            break;
+        case 3: //2 paralelas
+            let isBase3 = isBaseCase(a, b, c, d, e, f)
+            let isPerfSqr3 = isPerfctSquare(a, b, c, d, e, f)
+            if (isBase3) {
+                if (isBase3 == 1) { // x = -f/d
+                    board.create('line',[[-(f/d),0],[-(f/d),1]]);
+                }
+                else if (isBase3 == 2) { //x² = 0 ou x = -f/a
+                    board.create('line',[[-(f/a),0],[-(f/a),1]]);
+                }
+                else if (isBase3 == 3) { // y = -f/e
+                    board.create('line',[[0,-(f/e)],[1,-(f/e)]]);
+                }
+                else { //y² = 0 ou y = -f/c
+                    board.create('line',[[0,-(f/c)],[1,-(f/c)]]);
+                }
+            }
+            else if (isPerfSqr3) {
+                if (b > 0) { //(ax + cy)² = -f
+                    y0NegativeSqrt = -(Math.sqrt(-f)/c)
+                    y0PositiveSqrt = (Math.sqrt(-f)/c)
+                    y1NegativeSqrt = -(a + Math.sqrt(-f))/c
+                    y1PositiveSqrt = (Math.sqrt(-f) - a)/c
+
+                
+                    board.create('line',[[0,y0NegativeSqrt],[1,y1NegativeSqrt]]);
+                    board.create('line',[[0,y0PositiveSqrt],[1,y1PositiveSqrt]]); 
+                }
+                else { //(ax - cy)² = -f
+                    y0NegativeSqrt = -(Math.sqrt(-f)/c)
+                    y0PositiveSqrt = (Math.sqrt(-f)/c)
+                    y1NegativeSqrt = (a - Math.sqrt(-f))/c
+                    y1PositiveSqrt = (a + Math.sqrt(-f))/c
+
+                    board.create('line',[[0,y0NegativeSqrt],[1,y1NegativeSqrt]]);
+                    board.create('line',[[0,y0PositiveSqrt],[1,y1PositiveSqrt]]); 
+                }
+            }
+            // else /////////////////////////////////////////////////////////EU ACHO QUE NAO TEM CASO DE RETAS PARALELAS QUE NAO SEJA CASO BASE OU QUADRADO PERFEITO
+// http://www.onemathematicalcat.org/Math/Precalculus_obj/conicDiscriminant.htm
+// https://people.richland.edu/james/lecture/m116/conics/conics.html
+            break;
+        case 4: //intersecção
+            let isDiffSqrt = isDiffSquares(a,b,c,d,e,f)
+            if (isDiffSqrt) { // ax² - cy² = (√(a)x + √(c)y) * (√(a)x + √(c)y) = 0
+                y1SumCase = -Math.sqrt(Math.abs(a/c))
+                y1SubCase = Math.sqrt(Math.abs(a/c))
+
+                board.create('line',[[0,0],[1,y1SumCase]]);
+                board.create('line',[[0,0],[1,y1SubCase]]); 
+            }
+            // else /////////////////////////////////////////////////////////EU ACHO QUE NAO TEM CASO DE RETAS CONCORRENTES QUE NAO SEJA CASO BASE OU QUADRADO PERFEITO
+// http://www.onemathematicalcat.org/Math/Precalculus_obj/conicDiscriminant.htm
+// https://people.richland.edu/james/lecture/m116/conics/conics.html
+            break;
+        case 5: //line
+            let y0 = f
+            let y1 = a + b + c + d + e + f
+            board.create('line',[[0,y0],[1,y1]]);
+            break;
+        default:
+            board.create('conic', [a, c, f, b/2, d/2, e/2]);
+            // Ellipse
+            if (type == 6) {
+                v1 = Math.sqrt(((-f/a) > (-f/c)) ? (-f/a) : (-f/c))
+                v2 = -v1
+                if (a < c) {
+                    board.create('point',[v1,0],{name:'A1',size:0.1});
+                    board.create('point',[v2,0],{name:'A2',size:0.1});
+                }
+                else {
+                    board.create('point',[0,v1],{name:'A1',size:0.1});
+                    board.create('point',[0,v2],{name:'A2',size:0.1});
+                }
+
+                b1 = Math.sqrt(((-f/a) > (-f/c)) ? (-f/c) : (-f/a))
+                b2 = -b1
+                if (a < c) {
+                    board.create('point',[0,b1],{name:'B1',size:0.1});
+                    board.create('point',[0,b2],{name:'B2',size:0.1});
+                }
+                else {
+                    board.create('point',[b1,0],{name:'B1',size:0.1});
+                    board.create('point',[b2,0],{name:'B2',size:0.1});
+                }
+
+                f1 = Math.sqrt((v1**2) - (b1**2))
+                f2 = -f1
+                if (a < c) {
+                    board.create('point',[f1,0],{name:'F1',size:0.1});
+                    board.create('point',[f2,0],{name:'F2',size:0.1});
+                }
+                else {
+                    board.create('point',[0,f1],{name:'F1',size:0.1});
+                    board.create('point',[0,f2],{name:'F2',size:0.1});
+                }
+            }
+            // Hyperbola
+            else if (type == 7) {
+                let resultado = (-f/a > 0) ? (-f/a):(f/c);
+                resultado = Math.abs(resultado);
+
+                v1 = Math.sqrt(resultado);
+                v2 = -v1
+                if (a < c) {
+                    board.create('point',[v1,0],{name:'A1',size:0.1});
+                    board.create('point',[v2,0],{name:'A2',size:0.1});
+                }
+                else {
+                    board.create('point',[0,v1],{name:'A1',size:0.1});
+                    board.create('point',[0,v2],{name:'A2',size:0.1});
+                }   
+                
+                f1 = Math.sqrt(v1**2 + ((a>0)?(-f/c):(-f/a))**2)
+                f2 = -f1
+                if (a < c) {
+                    board.create('point',[f1,0],{name:'F1',size:0.1});
+                    board.create('point',[f2,0],{name:'F2',size:0.1});
+                }
+                else {
+                    board.create('point',[0,f1],{name:'F1',size:0.1});
+                    board.create('point',[0,f2],{name:'F2',size:0.1});
+                }
+            }
+            // Parabola
+            else if (type == 8) {
+                if (!a) {
+                    if (c>0) p = -d/(4*c);
+                    else p = d/(4*c);
+                    board.create('point',[p,0],{name:'F',size:0.1});
+                }
+                if (!c) {
+                    if (a>0) p = -e/(4*a);
+                    else p = e/(4*a);
+                    board.create('point',[0,p],{name:'F',size:0.1});
+                }
+               document.getElementById("Elements").innerText = string
+            }
+            break;
+    }
+}
+var board = JXG.JSXGraph.initBoard('jgxbox1', {boundingbox: [-20, 10, 20, -10], axis:true});
+    
+    // Pensei em fazer dois gráficos um pra plotar o gráfico inicial e outro pra plotar os
+    // os roles pós translação etc e tals
+
+/*
+43
+123
+13
+23
+4324
+2
+-------
+4
+0
+-9
+0
+0
+-36
+------
+4
+-4
+7
+12
+6
+-9
 */
